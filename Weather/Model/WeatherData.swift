5f6c55fd23e844507895dc5df9b11f9ea093885a
om-del <https://www.github.com/om-del>
2023-06-15 18:05:54 +0300

    Finished

--- Weather/Model/WeatherData.swift
+++ Weather/Model/WeatherData.swift
@@ -1,8 +1,17 @@
-//
-//  WeatherData.swift
-//  Weather
-//
-//  Created by mac on 15/06/2023.
-//
 
 import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
